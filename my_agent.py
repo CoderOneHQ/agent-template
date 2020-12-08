b968b20227d2bbe920860ee7be23e8b21d2b9746
Coder One <71113893+gocoderone@users.noreply.github.com>
2020-12-08 11:43:17 +1100

    Initial version

---
+++ my_agent.py
@@ -0,0 +1,47 @@
+'''
+
+TEMPLATE for creating your own Agent to compete in
+'Dungeons and Data Structures' at the Coder One AI Sports Challenge 2020.
+
+For more info and resources, check out: https://bit.ly/aisportschallenge
+
+'''
+
+# import any external packages by un-commenting them
+# if you'd like to test / request any additional packages - please check with the Coder One team
+import random
+# import time
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+class Agent:
+
+	def __init__(self):
+		'''
+		Place any initialisation code for your agent here (if any)
+
+		'''
+		pass
+
+	def next_move(self, game_state, player_state):
+		'''
+		This method is called each time your Agent is required to choose an action
+
+		If you're just starting out or are new to Python, you can place all your 
+		code within the ### CODE HERE ### tags. If you're more familiar with Python
+		and how classes and modules work, then go nuts. 
+		(Although we recommend that you use the Scrims to check your Agent is working)
+
+		'''
+
+		###### CODE HERE ######
+
+		# a list of all the actions your Agent can choose from
+		actions = ['','u','d','l','r','p']
+
+		# randomly choosing an action
+		action = random.choice(actions)
+
+		###### END CODE ######
+
+		return action
