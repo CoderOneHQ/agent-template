877f24bfee5c89cd836cacc784ccbee87ec3967a
abbyssoul <abbyssoul@gmail.com>
2020-12-12 14:36:57 +1100

    Fixed example vector-to-command mapping and added print for info

--- modular_agent/my_agent.py
+++ modular_agent/my_agent.py
@@ -17,11 +17,13 @@ class FreeRoamingAgent:
 		"""
 
 		# Lets pretend that agent is doing some thinking
-		time.sleep(0.2)
+		time.sleep(0.05)
 
 		free_tiles = agent_utils.get_free_tiles(location=player_state.location, game_state=game_state)
-
-		return self.move_to_tile(player_state.location, random.choice(free_tiles)) if free_tiles else ''
+		move = self.move_to_tile(player_state.location, random.choice(free_tiles)) if free_tiles else ''
+		print(f"For tick {game_state.tick_number} I'm going to move: {move}")
+		
+		return move
 
 	# given an adjacent tile location, move us there
 	def move_to_tile(self, location, tile):
@@ -31,8 +33,8 @@ class FreeRoamingAgent:
 		diff = tuple(y-x for x, y in zip(location, tile))
 
 		# return the action that moves in the direction of the tile
-		if diff == (0,1):		return 'u'
-		elif diff == (1,0):		return 'r'
-		elif diff == (0,-1):	return 'l'
-		elif diff == (-1,0):	return 'd'
+		if   diff == ( 1, 0):	return 'r'
+		elif diff == (-1, 0):	return 'l'
+		elif diff == ( 0, 1):	return 'u'
+		elif diff == ( 0,-1):	return 'd'
 		else:					return ''
